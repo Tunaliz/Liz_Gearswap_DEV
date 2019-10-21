44ca8e4f0e8a9b15d280f56cf69563b8f3e31816
Tunaliz <irtehlili@gmail.com>
2019-10-21 00:25:55 -0700

    v3

---
+++ Liz-Includes.lua
@@ -0,0 +1,9 @@
+version = '1.0'
+
+res = require('resources')
+texts = require('texts')
+
+include('Modes.lua')
+include('Liz-HelperFunctions.lua')
+include('Liz-HUD.lua')
+include('Liz-Mappings.lua')
